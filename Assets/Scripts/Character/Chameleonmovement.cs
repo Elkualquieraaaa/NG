b654a75b0531bf1fe39971b2aea04a27db4ce8dc
Elkualquieraaaa <triplehak88@gmail.com>
2024-11-17 19:04:36 -0500

    mas cambios

---
+++ Assets/Scripts/Character/Chameleonmovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chameleonmovement : MonoBehaviour
+{
+    Animator anim;
+    public float velocidad;
+    public Transform visionfield;
+    public Vector2 visionsize;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
